776bb76392358c90ec9b8569405754f0d116c9b7
amaljose <amaljvattakkunnel@gmail.com>
2023-08-04 21:07:02 +0530

    resetedStreak

--- lib/screens/user/screen_user.dart
+++ lib/screens/user/screen_user.dart
@@ -79,29 +79,6 @@ class _ScreenUserState extends State<ScreenUser> {
     await fetchCount();
   }
 
-  // @override
-  // void dispose() {
-  //   super.dispose();
-
-  //   lastDate = DateTime.now();
-  //   updateList(
-  //     widget.index,
-  //     StartModel(
-  //         id: DateTime.now().millisecond.toString(),
-  //         days: widget.totalDays,
-  //         habit: widget.habitName,
-  //         wheelCount: widget.wheelCount,
-  //         wheelName: widget.wheelName,
-  //         todayHours: habitNameNotifier.value.toString(),
-  //         today: daysNotifier.value.toString(),
-  //         streak: streakNotifier.value.toString(),
-  //         doitAt: widget.doItAt,
-  //         week: widget.week,
-  //         date: widget.date,
-  //         dateLastDone: lastDate),
-  //   );
-  // }
-
   Future<void> checkAndResetHabit() async {
     await resetNotifiers();
     lastDate = widget.lastDoneDate;
@@ -130,16 +107,75 @@ class _ScreenUserState extends State<ScreenUser> {
                 dateLastDone: currentDate),
           );
           getallDatas();
-          
-          habitName = 0; 
-            days = int.parse(widget.today);
-            streak = int.parse(widget.streak);
-            habitNameNotifier.value = 0;
-            daysNotifier.value = int.parse(widget.today);
-            streakNotifier.value = int.parse(widget.streak);
+
+          habitName = 0;
+          days = int.parse(widget.today);
+          streak = int.parse(widget.streak);
+          habitNameNotifier.value = 0;
+          daysNotifier.value = int.parse(widget.today);
+          streakNotifier.value = int.parse(widget.streak);
         },
       );
+      if (isOneDayOrMoreDifference(lastDate, currentDate)) {
+        streakNotifier.value = 0;
+        setState(
+          () {
+            updateList(
+              widget.index,
+              StartModel(
+                  id: DateTime.now().millisecond.toString(),
+                  days: widget.totalDays,
+                  habit: widget.habitName,
+                  wheelCount: widget.wheelCount,
+                  wheelName: widget.wheelName,
+                  todayHours: habitNameNotifier.value.toString(),
+                  today: daysNotifier.value.toString(),
+                  streak: streakNotifier.value.toString(),
+                  doitAt: widget.doItAt,
+                  week: widget.week,
+                  date: widget.date,
+                  dateLastDone: currentDate),
+            );
+            getallDatas();
+            habitName = 0;
+            days = int.parse(widget.today);
+            habitNameNotifier.value = 0; 
+            streakNotifier.value = 0;
+            daysNotifier.value = int.parse(widget.today);
+          }, 
+        );
+      }
     }
+  }
+
+  bool isOneDayOrMoreDifference(DateTime lastDate, DateTime currentDate) {
+    // Get the date components (day, month, year) for both dates
+    int lastDay = lastDate.day;
+    int currentDay = currentDate.day;
+
+    int lastMonth = lastDate.month;
+    int currentMonth = currentDate.month;
+
+    int lastYear = lastDate.year;
+    int currentYear = currentDate.year;
+
+    // Check if the year and month are the same
+    if (lastYear == currentYear && lastMonth == currentMonth) {
+      // Check if the difference between the days is 1 or more
+      return (currentDay - lastDay) >= 1;
+    }
+
+    // Check if the dates are 1 month apart or more
+    if ((currentYear == lastYear && currentMonth - lastMonth >= 1) ||
+        (currentYear - lastYear == 1 && currentMonth == 1 && lastMonth == 12)) {
+      // Check if the last date is the last day of the month and the current date is the first day of the month
+      bool isLastDayOfMonth =
+          (lastDate.day == DateTime(lastYear, lastMonth + 1, 0).day);
+      bool isFirstDayOfMonth = (currentDate.day == 1);
+      return isLastDayOfMonth && isFirstDayOfMonth;
+    }
+
+    return false;
   }
 
   Future<void> fetchCount() async {
