625516b87301cb5f126c1fc95067896495872efa
amaljose <amaljvattakkunnel@gmail.com>
2023-08-15 21:36:04 +0530

    start changed

--- lib/screens/start/intro_screens/intropage_1.dart
+++ lib/screens/start/intro_screens/intropage_1.dart
@@ -17,123 +17,58 @@ class IntroPageOne extends StatelessWidget {
       ),
       child: Column(
         children: [
-          const SizedBox(height: 60,),  
+          const SizedBox(
+            height: 60,
+          ),
           Text('Welcome to My Habits ',
-              style: GoogleFonts.dangrek( 
+              style: GoogleFonts.dangrek(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
                   color: Colors.white)),
-                  const SizedBox(height: 130,),  
+          const SizedBox(
+            height: 130,
+          ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.center, 
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text('Build the better you',
-                  style: GoogleFonts.comicNeue( 
+                  style: GoogleFonts.comicNeue(
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
                       color: Colors.white)),
             ],
           ),
-          const SizedBox(height: 30,), 
+          const SizedBox(
+            height: 30,
+          ),
           Column(
-
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                
-                children: [
-                  const SizedBox(
-                    width: 30, 
-                  ),
-                  const Icon(
-                    Icons.file_download_done_sharp,
-                    color: Colors.white,
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Text('Plan a habit for daily',
-                      style: GoogleFonts.urbanist(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white)),
-                ],
-              ),
-              Row(
-                children: [
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  const Icon(
-                    Icons.radar_outlined,
-                    color: Colors.white,
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Text('Track habits',
-                      style: GoogleFonts.urbanist(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white)),
-                ],
-              ),
-              Row(
-                children: [
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  const Icon(
-                    Icons.map,
-                    color: Colors.white,
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Text('Build routines',
-                      style: GoogleFonts.urbanist(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white)),
-                ],
-              ),
-              Row(
-                children: [
-                  const SizedBox(
-                   width: 30,
-                  ),
-                  const Icon(
-                    Icons.celebration,
-                    color: Colors.white,
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Text('Accomplish goals',
-                      style: GoogleFonts.urbanist(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white)),
-                ],
-              ),
-              Row(
-                children: [
-                  const SizedBox(
-                    width: 30, 
-                  ),
-                  const Icon(
-                    Icons.health_and_safety,
-                    color: Colors.white,
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Text('Get your life in order',
-                      style: GoogleFonts.urbanist(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white)),
-                ],
-              ),
+              Text('Plan a habit for daily',
+                  style: GoogleFonts.urbanist(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+              Text('Track habits',
+                  style: GoogleFonts.urbanist(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+              Text('Build routines',
+                  style: GoogleFonts.urbanist(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+              Text('Accomplish goals',
+                  style: GoogleFonts.urbanist(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+              Text('Get your life in order',
+                  style: GoogleFonts.urbanist(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
             ],
           )
         ],
